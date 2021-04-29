4274fbc278cc586a6ecc70529cb3ee347cfe567d
Javlonbek Rajabov <71313500+radjabov4443@users.noreply.github.com>
2021-04-29 15:03:44 +0500

    added question component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { QuestionComponent } from './question.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
