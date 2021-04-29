d216e8abe2be749ec990f4b4d27b526c1cf629e5
Javlonbek Rajabov <71313500+radjabov4443@users.noreply.github.com>
2021-04-29 15:36:57 +0500

    Added angular material and question form

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,7 +15,11 @@ import { QuestionComponent } from './question.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
